67890645840f3dfe14bb2429ca856228b587e18a
hamudal <hamudal.hd@gmail.com>
2025-06-09 08:02:46 +0200

    delete test

--- tests/conftest.py
+++
@@ -1,8 +0,0 @@
-# tests/conftest.py
-import sys
-import os
-
-# Füge das Projektverzeichnis (Parent von tests/) zum sys.path hinzu
-root = os.path.abspath(os.path.join(os.path.dirname(__file__), os.pardir))
-if root not in sys.path:
-    sys.path.insert(0, root)
